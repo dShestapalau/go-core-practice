1e8ee4427f1daab31cfc41d9ec59cc5393f27398
dzianisshestapalau <dzianis.shestapalau@gmail.com>
2024-09-30 22:52:47 +0300

    Structs, methods, pointers

--- main.go
+++ main.go
@@ -1,23 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"example.com/practice-project/prices"
+)
 
 func main() {
-	var prices []float64 = []float64{10, 20, 30}
-
 	var taxRates []float64 = []float64{0, 0.07, 0.1, 0.15}
 
-	result := make(map[float64][]float64)
-
 	for _, taxRate := range taxRates {
-		var taxInducedPrices = make([]float64, len(prices))
-
-		for priceIndex, price := range prices {
-			taxInducedPrices[priceIndex] = price * (1 + taxRate)
-		}
-
-		result[taxRate] = taxInducedPrices
+		priceJob := prices.New(taxRate)
+		priceJob.Process()
 	}
-
-	fmt.Println(result)
 }
